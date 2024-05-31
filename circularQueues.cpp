c256756381ee8f432903ff5f5042e461b9b9fb6b
Yurcell <chestayurcel@gmail.com>
2024-05-31 15:01:43 +0700

    menambahkan logika untuk menampilkan isi dari queue jika queue tidak kosong dalam metode display pada kelas Queues. serta memundurkan line 8 - line 33

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -5,32 +5,32 @@ class Queues {
     int FRONT, REAR, max = 5;
     int queue_array[5];
 
-    public:
-        Queues() {
-            FRONT = -1;
-            REAR = -1;
+public:
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert() {
+        int num;
+        cout << "Enter a number : ";
+        cin >> num;
+        cout << endl;
+
+        if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR +1)) {
+        cout << "\nQueue overflow\n";
+        return;
         }
-
-        void insert() {
-            int num;
-            cout << "Enter a number : ";
-            cin >> num;
-            cout << endl;
-
-            if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR +1)) {
-            cout << "\nQueue overflow\n";
-            return;
-            }
     
-            if (FRONT == -1) {
-                FRONT = 0;
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else {
+            if (REAR == max -1)
                 REAR = 0;
-            }
-            else {
-                if (REAR == max -1)
-                    REAR = 0;
-                else
-                    REAR = REAR + 1;
+            else
+                REAR = REAR + 1;
         }
         queue_array[REAR] = num;
     }
@@ -40,7 +40,7 @@ class Queues {
             cout << "Queue underflow\n";
             return;
         }
-        cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << "\n"
+        cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << "\n";
 
         if (FRONT == REAR) {
             FRONT = -1;
@@ -63,6 +63,13 @@ class Queues {
         }
 
         cout << "\nElements in the queue are...\n";
-        
+
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 }
