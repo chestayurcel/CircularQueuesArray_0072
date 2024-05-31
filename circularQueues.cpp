d356bdfe439f01c93b29d3086373ca8a0502c1e1
Yurcell <chestayurcel@gmail.com>
2024-05-31 14:43:58 +0700

    menambahkan elemen ke array queue_array di indeks REAR dalam kelas Queues

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -5,31 +5,33 @@ class Queues {
     int FRONT, REAR, max = 5;
     int queue_array[5];
 
-public:
-    Queues() {
-        FRONT = -1;
-        REAR = -1;
+    public:
+        Queues() {
+            FRONT = -1;
+            REAR = -1;
     }
 
-    void insert() {
-        int num;
-        cout << "Enter a number : ";
-        cin >> num;
-        cout << endl;
+        void insert() {
+            int num;
+            cout << "Enter a number : ";
+            cin >> num;
+            cout << endl;
 
-        if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR +1)) {
-        cout << "\nQueue overflow\n";
-        return;
+            if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR +1)) {
+            cout << "\nQueue overflow\n";
+            return;
         }
     
-        if (FRONT == -1) {
-            FRONT = 0;
-            REAR = 0;
-        }
-        else {
-            if (REAR == max -1)
+            if (FRONT == -1) {
+                FRONT = 0;
                 REAR = 0;
-            else
-                REAR = REAR + 1;
         }
+            else {
+                if (REAR == max -1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
 }
