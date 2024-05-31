153bd1f52e9e27f62dbe7e0d3ca88e5d1e47c95e
Yurcell <chestayurcel@gmail.com>
2024-05-31 15:03:55 +0700

    menambahkan logika tambahan untuk menampilkan isi dari queue jika queue tidak kosong dan melintasi batas array dalam metode display pada kelas Queues

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -71,5 +71,13 @@ public:
             }
             cout << endl;
         }
+        else {
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+        }
     }
 }
