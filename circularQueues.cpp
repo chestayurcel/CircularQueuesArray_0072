d1437b5a83e2c0ac030d75263dbb256464623710
Yurcell <chestayurcel@gmail.com>
2024-05-31 14:48:51 +0700

    membuat output text yang memberitahukan elemen mana yang dihapus

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -40,5 +40,6 @@ class Queues {
             cout << "Queue underflow\n";
             return;
         }
+        cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << "\n"
     }
 }
