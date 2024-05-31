f08991ea0b89e4b5350ac3cf5cd096cecc81a5fd
Yurcell <chestayurcel@gmail.com>
2024-05-31 14:51:46 +0700

    menambahkan logika untuk memperbarui FRONT setelah menghapus elemen dalam metode remove pada kelas Queues

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -41,5 +41,15 @@ class Queues {
             return;
         }
         cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << "\n"
+
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else {
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else FRONT = FRONT + 1;
+        }
     }
 }
