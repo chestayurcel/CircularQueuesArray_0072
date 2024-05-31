647156786457307760bbb9c1d24e142a9c4d8760
Yurcell <chestayurcel@gmail.com>
2024-05-31 14:55:16 +0700

    menambahkan pengecekan untuk menampilkan pesan jika queue kosong dalam metode 'display' pada kelas Queues

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -56,6 +56,10 @@ class Queues {
     void display() {
         int FRONT_position = FRONT;
         int REAR_position = REAR;
-        
+
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
     }
 }
